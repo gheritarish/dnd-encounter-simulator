1787b3f73709ec85282085348685045c73cae6a2
gheritarish <ilissiae@gmail.com>
2021-08-28 17:28:28 +0200

    :sparkles:  add conversion from stats to modifiers + crits

--- DndEncounterSimulator/Objects/Creature.py
+++ DndEncounterSimulator/Objects/Creature.py
@@ -2,6 +2,8 @@ from typing import Dict
 
 import dice
 
+from DndEncounterSimulator.Objects.utils.conversion import convert_stat_to_mod
+
 
 class Creature:
     """
@@ -21,6 +23,9 @@ class Creature:
                 "charisma": int(stats["charisma"]),
             }
         self.weapons = {"damage": "1d6"}
+        self.modifiers = {
+            key: convert_stat_to_mod(value) for (key, value) in self.stats.items()
+        }
 
 
 class Monster(Creature):
@@ -35,12 +40,30 @@ class Monster(Creature):
         self.proficiency = int(proficiency)
 
     def attack(self, opponent: Creature):
-        to_hit = dice.roll("1d20")[0] + self.proficiency + self.stats["strength"]
+        """
+        Method simulating the attack of a monster on a Creature
+
+        :param opponent: (Creature) The target of the attack.
+        """
+        critical_hit = False
+
+        dice_roll = dice.roll("1d20")[0]
+        to_hit = dice_roll + self.proficiency + self.modifiers["strength"]
+
         if to_hit >= opponent.armor_class:
             hit = True
+            if dice_roll == 20:
+                critical_hit = True
         else:
             hit = False
         if hit:
-            opponent.hit_points -= (
-                dice.roll(self.weapons["damage"])[0] + self.stats["strength"]
-            )
+            if critical_hit:
+                opponent.hit_points -= (
+                    dice.roll(self.weapons["damage"])[0]
+                    + dice.roll(self.weapons["damage"])[0]
+                    + self.modifiers["strength"]
+                )
+            else:
+                opponent.hit_points -= (
+                    dice.roll(self.weapons["damage"])[0] + self.modifiers["strength"]
+                )
