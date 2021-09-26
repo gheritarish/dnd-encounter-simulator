31fea382ddf611e9a166da77f9af2db787252013
Gheritarish <ilissiae@gmail.com>
2021-09-26 14:57:30 +0200

    :sparkles:  Add script to initialize the database schema

---
+++ Database/initialize_database.py
@@ -0,0 +1,62 @@
+import argparse
+import sys
+
+from loguru import logger
+
+from DndEncounterSimulator.Objects.Database import Database
+
+
+def define_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--database", type=str, help="The name of the database", default=None
+    )
+    parser.add_argument(
+        "--user", "-u", type=str, help="The identifiant of the user", default="postgres"
+    )
+    parser.add_argument(
+        "--port",
+        "-p",
+        type=int,
+        help="The port to connect to the database",
+        default=5432,
+    )
+    parser.add_argument(
+        "--password",
+        type=str,
+        help="The password to connect to the database",
+        default=None,
+    )
+    parser.add_argument(
+        "--host", type=str, help="The host of the database", default="localhost"
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = define_args()
+    database = Database(
+        user=args.user,
+        database=args.database,
+        password=args.password,
+        host=args.host,
+        port=args.port,
+    )
+
+    if database.is_valid():
+        sql_file = open("./database_initialization.sql")
+        sql_as_string = sql_file.read()
+        try:
+            database.execute_query(sql_as_string)
+            database.commit()
+        except Exception as error:
+            database.rollback()
+            logger.warning(error)
+    else:
+        logger.error("Failed to connect to the database")
+        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
